4840ff58f40843a224e3a0e8aa27a9e0c836678e
Realhyeonwoo <gusdn7269@naver.com>
2020-05-13 09:53:37 +0900

    boj

--- 03.삼성기출/17136.cpp
+++ 03.삼성기출/17136.cpp
@@ -1,102 +1,169 @@
-#include<iostream> 
+#include<iostream>
 #include<vector>
 #include<algorithm>
 
 #define MAX 10
+#define INF 9875654321
 using namespace std;
 
-int Answer = 987564321, Total_Cnt;
-int map[MAX][MAX];
+int map[MAX][MAX], Answer = INF;
 bool visited[MAX][MAX];
-bool Already_Answer = true;
 vector<pair<int, int> > v;
 
-bool Can_Fill(int x, int y, int r) {
-	int Cnt = 0; 
-	for(int i=x; i<x+r; i++)	 {
+bool canFill(int y, int x, int r) {
+	int sum = 0;
+	for(int i=y; i<y+r; i++) {
 		if(i >= MAX) break;
-		for(int j=y; j<y+r; j++) {
+		for(int j=x; j<x+r; j++) {
 			if(j >= MAX) break;
-			if(map[i][j] == 1 && !visited[i][j]) cnt++;
+			if(map[i][j] == 1) sum++;
 		}
 	}
 	
-	if(cnt == r*r) return true;
-	else return false;
+	return sum == r*r ? true : false;
 }
 
-inf Find_Range(int x, int y) {
-	if(Can_Fill(x, y, 2)){
-		if(Can_Fill(x, y, 3)) {
-			if(Can_Fill(x, y, 4)) {
-				if(Can_Fill(x, y, 5)) {
+int checkSize(int y, int x) {
+	if(canFill(y, x, 2))	{
+		if(canFill(y, x, 3))	{
+			if(canFill(y, x, 4))	{
+				if(canFill(y, x, 5))	{
 					return 5;
 				}
 				return 4;
 			}
 			return 3;
-		}
+		}	
 		return 2;
 	}
 	return 1;
 }
 
-void Make_Visit(int x, int y, int r, bool t) {
-	for (int i = x; i < x + r; i++) {
-		for (int j = y; j < y + r; j++) {
-			Visit[i][j] = t;
+void makeVisit(int y, int x, int r, bool b) {
+	for(int i=y; i<y+r; i++) {
+		for(int j=x; j<x+r; j++) {
+			visited[i][j] = b;
 		}
-   }
+	}
 }
-void DFS(int idx, int One, int Two, int Three, int Four, int Five, int Total) {
-	int Use = One + Two + Three + Four + Five;
+
+void dfs(int idx, int One, int Two, int Three, int Four, int Five, int totalCnt) {
+	int usedCount = One + Two + Three + Four + Five;
 	
-	if(Answer < Use) return;
-	if(Total == Total_Cnt) {
-		Answer = min(Answer, Use);
+	if(Answer < usedCount) return;
+	
+	if(totalCnt == v.size()) {
+		Answer = min(Answer, usedCount);
 		return;
 	}
 	
-	if(visited[v[idx].first][v[i].second]) {
-		dfs(idx+1, One, Two, Three, Four, Five, Total);
-		return; 
+	if(visited[v[idx].first][v[idx].second]) {
+		dfs(idx+1, One, Two, Three, Four, Five, totalCnt);
+		return;
 	}
 	
-	int Can_Fill = Find_Range(v[idx].first, v[idx].second);
-	if(Can_Fill == 1) {
-		if(One + 1 <= 5) {
-			Make_Visit(v[idx].first, v[idx].second, 1, true);
-			DFS(idx+1, One+1, Two, Three, Four, Five, Total+1);
-			Make_Visit(v[idx].first, v[idx].second, 1, false);
+	int size = checkSize(v[idx].first, v[idx].second);
+	if(size == 1) {
+		if(One+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 1, true);
+			dfs(idx+1, One+1, Two, Three, Four, Five, totalCnt+1);
+			makeVisit(v[idx].first, v[idx].second, 1, false);
 		}
-	} else if(Can_Fill == 2) {
-		
-	} else if(Can_Fill == 3) {
-	} else if(Can_Fill == 4) {
-	} else if(Can_Fill == 5) {
+	} else if(size == 2) {
+		if(Two+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 2, true);
+			dfs(idx+1, One, Two+1, Three, Four, Five, totalCnt+4);
+			makeVisit(v[idx].first, v[idx].second, 2, false);
+		}
+		if(One+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 1, true);
+			dfs(idx+1, One+1, Two, Three, Four, Five, totalCnt+1);
+			makeVisit(v[idx].first, v[idx].second, 1, false);
+		}
+	} else if(size == 3) {
+		if(Three+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 3, true);
+			dfs(idx+1, One, Two, Three+1, Four, Five, totalCnt+9);
+			makeVisit(v[idx].first, v[idx].second, 3, false);
+		}
+		if(Two+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 2, true);
+			dfs(idx+1, One, Two+1, Three, Four, Five, totalCnt+4);
+			makeVisit(v[idx].first, v[idx].second, 2, false);
+		}
+		if(One+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 1, true);
+			dfs(idx+1, One+1, Two, Three, Four, Five, totalCnt+1);
+			makeVisit(v[idx].first, v[idx].second, 1, false);
+		}
+	} else if(size == 4) {
+		if(Four+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 4, true);
+			dfs(idx+1, One, Two, Three, Four+1, Five, totalCnt+16);
+			makeVisit(v[idx].first, v[idx].second, 4, false);
+		}
+		if(Three+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 3, true);
+			dfs(idx+1, One, Two, Three+1, Four, Five, totalCnt+9);
+			makeVisit(v[idx].first, v[idx].second, 3, false);
+		}
+		if(Two+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 2, true);
+			dfs(idx+1, One, Two+1, Three, Four, Five, totalCnt+4);
+			makeVisit(v[idx].first, v[idx].second, 2, false);
+		}
+		if(One+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 1, true);
+			dfs(idx+1, One+1, Two, Three, Four, Five, totalCnt+1);
+			makeVisit(v[idx].first, v[idx].second, 1, false);
+		}
+	} else if(size == 5) {
+		if(Five+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 5, true);
+			dfs(idx+1, One, Two, Three, Four, Five+1, totalCnt+25);
+			makeVisit(v[idx].first, v[idx].second, 5, false);
+		}
+		if(Four+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 4, true);
+			dfs(idx+1, One, Two, Three, Four+1, Five, totalCnt+16);
+			makeVisit(v[idx].first, v[idx].second, 4, false);
+		}
+		if(Three+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 3, true);
+			dfs(idx+1, One, Two, Three+1, Four, Five, totalCnt+9);
+			makeVisit(v[idx].first, v[idx].second, 3, false);
+		}
+		if(Two+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 2, true);
+			dfs(idx+1, One, Two+1, Three, Four, Five, totalCnt+4);
+			makeVisit(v[idx].first, v[idx].second, 2, false);
+		}
+		if(One+1 <= 5) {
+			makeVisit(v[idx].first, v[idx].second, 1, true);
+			dfs(idx+1, One+1, Two, Three, Four, Five, totalCnt+1);
+			makeVisit(v[idx].first, v[idx].second, 1, false);
+		}
 	}
 }
 
 int main(void) {
-	for(int i=0; i<MAX; i++) {
-		for(int j=0; j<MAX; j++) {
-			cin >> map[i][j];
-			if(map[i][j] == 1) {
-				v.push_back(make_pair(i, j));
-				Total_Cnt++;
-				Already_Answer = false;
-			}
+	// INPUT
+	for(int y=0; y<MAX; y++) {
+		for(int x=0; x<MAX; x++) {
+			scanf("%d", &map[y][x]);
+			if(map[y][x]) v.push_back(make_pair(y, x));
 		}
 	}
 	
-	if(Already_Answer) {
-		cout << 0 << endl;
-		return;
+	// SIMULATION
+	if(v.size() == 0) {
+		Answer = 0;
+	} else {
+		dfs(0, 0, 0, 0, 0, 0, 0);
+		if(Answer == INF) Answer = -1;
 	}
 	
-	DFS(0, 0, 0, 0, 0, 0, 0);
-	
-	if(Answer == 987564321) Answer = -1;
-	cout << Answer << endl;
+	// OUTPUT
+	printf("%d\n", Answer);
 	return 0;
 }
--- 03.삼성기출/6987.cpp
+++ 03.삼성기출/6987.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int map[ROW][COL], valid;
 vector<int> Answer;
 
-// 0 1 2 3 4 5 
+// 0 1 2 3 4 5
 void dfs(int t1, int t2, int game[]) {
-	if(t1 > 4) {
+	if(t1 == 5) {
 		for(int i=0; i<COL; i++) {
 			if(game[i] > 0) return;
 		}
@@ -19,10 +19,9 @@ void dfs(int t1, int t2, int game[]) {
 	
 	if(t2 == 6) {
 		dfs(t1+1, t1+2, game);
-		return;
 	}
 	
-	if(game[t1*3 + 0] > 0 && game[t2*3 + 2] > 0) {
+	if(game[t1*3 + 0] > 0 && game[t2*3 +2] > 0) {
 		game[t1*3 + 0]--;
 		game[t2*3 + 2]--;
 		dfs(t1, t2+1, game);
@@ -30,7 +29,7 @@ void dfs(int t1, int t2, int game[]) {
 		game[t2*3 + 2]++;
 	}
 	
-	if(game[t1*3 + 1] > 0 && game[t2*3 + 1] > 0) {
+	if(game[t1*3 + 1] > 0 && game[t2*3 +1] > 0) {
 		game[t1*3 + 1]--;
 		game[t2*3 + 1]--;
 		dfs(t1, t2+1, game);
@@ -38,14 +37,13 @@ void dfs(int t1, int t2, int game[]) {
 		game[t2*3 + 1]++;
 	}
 	
-	if(game[t1*3 + 2] > 0 && game[t2*3 + 0] > 0) {
+	if(game[t1*3 + 2] > 0 && game[t2*3 +0] > 0) {
 		game[t1*3 + 2]--;
 		game[t2*3 + 0]--;
 		dfs(t1, t2+1, game);
 		game[t1*3 + 2]++;
 		game[t2*3 + 0]++;
 	}
-	
 }
 
 int main(void) {
@@ -56,13 +54,12 @@ int main(void) {
 		}
 	}
 	
-	// SIMULATION
+	// SIMUALTION
 	for(int y=0; y<ROW; y++) {
 		valid = 0;
 		dfs(0, 1, map[y]);
 		Answer.push_back(valid);
 	}
-	
 	
 	// OUTPUT
 	for(int i=0; i<Answer.size(); i++) {
