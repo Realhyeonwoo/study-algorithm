43e738ad9c0d78ba5891a49e8ca1080ef7bd805b
Realhyeonwoo <gusdn7269@naver.com>
2020-06-15 09:57:21 +0900

    study algo

--- 백준문제/14502.cpp
+++ 백준문제/14502.cpp
@@ -1,92 +1,104 @@
 #include<iostream>
 #include<queue>
-#include<algorithm>
+#include<vector>
+#define MAX 8
 using namespace std;
 
-int n, m, ret;
-int map[8][8];
+int N, M, Answer = -987654321; // 세로(N) 가로(M)
+int map[MAX][MAX], cpyMap[MAX][MAX]; // 빈칸(0) 벽(1) 바이러스(2) 
+bool visited[MAX][MAX], visit[MAX*MAX];
+queue<pair<int, int> > virusQ;
+vector<pair<int, int> > emptyV;
 
-const int dy[] = { -1, 1, 0 , 0 };
-const int dx[] = { 0, 0, -1, 1 };
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
 
-void bfs() {
-	queue<int> q;
-	int backup[8][8];
-	int visited[8][8] = { 0, };
-	for(int y=0; y<n; ++y) {
-		for(int x=0; x<m; ++x) {
-			backup[y][x] = map[y][x];
-			if(backup[y][x] == 2) {
-				q.push(y*10 + x);
-			}
+void copyMap(void) {
+	for(int y=0; y<MAX; y++) {
+		for(int x=0; x<MAX; x++) {
+			cpyMap[y][x] = map[y][x];
 		}
-	}
-	
-	while(!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		
-		int cy = cur / 10;
-		int cx = cur % 10;
-		
-		backup[cy][cx] = 2;
-		
-		for(int dir=0; dir<4; ++dir) {
-			int ny = cy + dy[dir]; 
-			int nx = cx + dx[dir];
-			
-			if(ny<0 || ny>=n || nx<0 || nx>=m) {
-				continue;
-			}
-			
-			if(visited[ny][nx]==0 && backup[ny][nx]==0) {
-				visited[ny][nx] = 1;
-				q.push(ny * 10 + nx);
-			}
-		}
-	}
-	int candi = 0; 
-	for(int y=0; y<n; ++y) {
-		for(int x=0; x<m; ++x) {
-			if(backup[y][x] == 0) {
-				++candi;
-			}
-		}
-	}
-	
-	if(ret < candi) {
-		ret = candi;
 	}
 }
 
-void pick_dfs(int count, int sy, int sx) {
-	if(count == 3) {
-		bfs(); 
-		return;
-	}
+void spreadVirus(void) {
+	queue<pair<int, int> > Q = virusQ;
 	
-	for(int y=sy; y<n; ++y) {
-		for(int x=sx; x<m; ++x) {
-			if(map[y][x] == 0) {
-				map[y][x] = 1; // 벽으로 채움 
-				pick_dfs(count+1, y, x);
-				map[y][x] = 0;
-			}
+	while(!Q.empty()) {
+		int y = Q.front().first;
+		int x = Q.front().second;
+		Q.pop();
+		
+		for(int dir=0; dir<4; dir++) {
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
+			
+			if(ny<0 || ny>=N || nx<0 || nx>=M) continue;
+			if(cpyMap[ny][nx] == 1 || visited[ny][nx]) continue;
+			
+			cpyMap[ny][nx] = 1;
+			visited[ny][nx] = true;
+			Q.push(make_pair(ny, nx));
 		}
-		sx = 0; 
 	}
 }
-int main() {
-	scanf("%d %d", &n, &m);
-	for(int y=0; y<n; ++y) {
-		for(int x=0; x<m; ++x) {
-			scanf("%d", &map[y][x]);
+
+int countSafeArea(void) {
+	int res = 0;
+	for(int y=0; y<N; y++){
+		for(int x=0; x<M; x++) {
+			if(cpyMap[y][x] == 0) res++;
+		}
+	}
+	return res;
+}
+
+void putWall_dfs(int start, int cnt) {
+	if(cnt == 3) {
+		copyMap();
+		for(int i=0; i<emptyV.size(); i++) {
+			if(visit[i]) {
+				int y = emptyV[i].first;
+				int x = emptyV[i].second;
+				cpyMap[y][x] = 1;
+			}
+		}
+		spreadVirus();		
+		Answer = max(Answer, countSafeArea());
+		for(int y=0; y<N; y++) fill(visited[y], visited[y] + MAX, false);
+		return; 
+	}
+	
+	for(int i=start; i<emptyV.size(); i++) {
+		if(visit[i]) continue;
+		visit[i] = true;
+		putWall_dfs(i, cnt + 1);
+		visit[i] = false;
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	// INPUT
+	cin >> N >> M;
+	for(int y=0; y<N; y++){
+		for(int x=0; x<M; x++) {
+			cin >> map[y][x];
+			if(map[y][x] == 0) emptyV.push_back(make_pair(y, x));
+			else if(map[y][x] == 2) {
+				virusQ.push(make_pair(y, x));
+				visited[y][x] = true;
+			}
 		}
 	}
 	
-	pick_dfs(0, 0, 0);
+	// SIMUALATION
+	putWall_dfs(0, 0);
 	
-	printf("%d\n", ret);
+	// OUTPUT
+	cout << Answer;
 	
 	return 0;
 }
